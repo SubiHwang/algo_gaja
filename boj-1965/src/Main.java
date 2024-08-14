a0e550842714eb60b34ca08a6683cf7d3f0f4b81
Subi Hwang <ghkdtnql@gmail.com>
2024-08-14 10:04:12 +0900

    1965: dp

--- boj-1965/src/Main.java
+++ boj-1965/src/Main.java
@@ -1,5 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	static int N;
+	static int[] map;
+	static int[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+
+		map = new int[N + 1];
+		dp = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			map[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.fill(dp, 1); //모든 최솟값은 1이다.
+
+		int max = 0;
+		for (int i = 2; i <= N; i++) {
+			for (int j = 1; j < i; j++) {
+				if (map[i] > map[j])
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+			}
+			max = Math.max(dp[i], max);
+		}
+
+		System.out.println(max);
+
 	}
+
 }
